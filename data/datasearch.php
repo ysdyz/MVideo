c36bedd28c30cacf03ca9fbe430fddbc047133a3
MHanL <mlooc@qq.com>
2018-07-09 19:27:43 +0800

    解决页面顶部留白

--- data/datasearch.php
+++ data/datasearch.php
@@ -1,96 +1,174 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Filmy
  * Date: 2018/6/30
  * Time: 13:28
  */
-
+header('Content-type:text/json');
 include("readcontent.class.php");
+
 include('pwd.php');
+
 function read_info()
+
 {
+
     $readcontent = new Read_content();
+
     $key = isset($_GET['key']) ? $_GET['key'] : '';
+
     $url = "http://zuida.me/index.php?m=vod-search&wd=" . $key;
+
     $listcontent = $readcontent->MloocCurl($url);
+
     if (stristr($listcontent, '请不要频繁操作，时间间隔为5秒') == false) {
+
         $ruleMatchDetailInList = "~<a href=\"(.*?)\" target=\"_blank\">(.*?)<\/span><\/a><\/span>~";#正则表达式
+
         preg_match_all($ruleMatchDetailInList, $listcontent, $title_id);
 
+
         $ruleMatchDetailInList = "~<span class=\"xing_vb5\">(.*?)<\/span>~";#正则表达式
+
         preg_match_all($ruleMatchDetailInList, $listcontent, $category);
 
+
         $ruleMatchDetailInList = "~<span class=\"xing_vb[6-7]\">(.*?)<\/span>~";#正则表达式
+
         preg_match_all($ruleMatchDetailInList, $listcontent, $date);
+
         $json = '';
+
         $xunhuan = count($title_id[1]);
+
         for ($i = 0; $i < $xunhuan; $i++) {
+
             $title_id[1][$i] = preg_replace('/\/\?m=vod-detail-id-(\d+)\.html/i', '$1', $title_id[1][$i]);
+
             //        print_r("<li><span class='title' ><a href='javascript:void(0);' data-id='" . $title_id[1][$i] . "'>" . $title_id[2][$i] . "</a></span><span class='category'>" . $category[1][$i] . "</span><span class='date'>" . $date[1][$i] . "</span></li>");
+
             if ((stristr($category[1][$i], '福利') != false) || (stristr($category[1][$i], '伦理片') != false)) {
 
+
             } else {
+
                 #}{"title
+
                 $json = $json . json_encode(array('title' => $title_id[2][$i], 'id' => $title_id[1][$i], 'category' => $category[1][$i], 'date' => $date[1][$i]));
+
             }
+
         }
+
         if ($json == '') {
+
             echo "{\"status\":\"error\"}";
+
         } else {
+
             echo "{\"status\":\"success\",\"result\":[";
+
             echo str_replace('}{"title', '},{"title', $json);
+
             echo "]}";
+
         }
+
     } else {
+
         echo "{\"status\":\"error\"}";
+
     }
 
+
 }
+
 
 function read_infoTwo()
+
 {
+
     $readcontent = new Read_content();
+
     $key = isset($_GET['key']) ? $_GET['key'] : '';
+
     $url = "http://zuida.me/index.php?m=vod-search&wd=" . $key;
+
     $listcontent = $readcontent->MloocCurl($url);
+
     if (stristr($listcontent, '请不要频繁操作，时间间隔为5秒') == false) {
+
         $ruleMatchDetailInList = "~<a href=\"(.*?)\" target=\"_blank\">(.*?)<\/span><\/a><\/span>~";#正则表达式
+
         preg_match_all($ruleMatchDetailInList, $listcontent, $title_id);
 
+
         $ruleMatchDetailInList = "~<span class=\"xing_vb5\">(.*?)<\/span>~";#正则表达式
+
         preg_match_all($ruleMatchDetailInList, $listcontent, $category);
 
+
         $ruleMatchDetailInList = "~<span class=\"xing_vb[6-7]\">(.*?)<\/span>~";#正则表达式
+
         preg_match_all($ruleMatchDetailInList, $listcontent, $date);
+
         $json = '';
+
         $xunhuan = count($title_id[1]);
+
         for ($i = 0; $i < $xunhuan; $i++) {
+
             $title_id[1][$i] = preg_replace('/\/\?m=vod-detail-id-(\d+)\.html/i', '$1', $title_id[1][$i]);
 
+
             $json = $json . json_encode(array('title' => $title_id[2][$i], 'id' => $title_id[1][$i], 'category' => $category[1][$i], 'date' => $date[1][$i]));
+
         }
+
         if ($json == '') {
+
             echo "{\"status\":\"error\"}";
+
         } else {
+
             echo "{\"status\":\"success\",\"result\":[";
+
             echo str_replace('}{"title', '},{"title', $json);
+
             echo "]}";
+
         }
+
     } else {
+
         echo "{\"status\":\"error\"}";
+
     }
 
+
 }
+
 
 if (isset($_COOKIE["fulilunliju"])) {
+
     if ($_COOKIE["fulilunliju"] != $password) {
+
         read_info();
+
         exit();
+
     }
+
 } else {
+
     read_info();
+
     exit();
+
 }
+
 read_infoTwo();
+
 ?>
--- data/readcontent.class.php
+++ data/readcontent.class.php
@@ -1,30 +1,57 @@
 <?php
+
 /**
+
  * Created by PhpStorm.
+
  * User: Filmy
+
  * Date: 2018/6/28
+
  * Time: 14:59
+
  */
 
+
+
 class Read_content
+
 {
+
     public function MloocCurl($url)
+
     {
+
         $UserAgent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36';#设置UserAgent
+
         $curl = curl_init();
+
         curl_setopt($curl, CURLOPT_URL, $url);
+
         curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);
+
         #关闭SSL
+
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+
         #返回数据不直接显示
+
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
         $response = curl_exec($curl);
+
         curl_close($curl);
+
         return $response;
+
     }
+
 }
 
 
-?>
 
+
+
+?>
